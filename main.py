4624dcd6c5d83895a126d0827be2cb69d1c11164
= <=>
2020-07-17 21:54:48 +0200

    [Userinterface angepasst]

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import socket
 import binascii
 
-ip = '192.168.1.100'
-port = 5577
-buffer_size = 1024
-on = b'71230fa3'
-off = b'71240fa4'
-switchcolor = b'6125070f9c'
+class controler:
+    
+    def __init__(self):
+    
+        self.colors = {
+                        1: b'71230fa3',
+                        2: b'71240fa4',
+                        3: b'6125070f9c',
+                        4: b'31ff880d00000fd4'
+                    }
+    
+    def control(self, ip, port=5577):
+        
+        chg = input('1: on\n2: off\n3: Switch color\n4: soft yellow tone\n')
+        
+        chg = int(chg)
+        
+        if chg <= 0 or chg > len(self.colors):
+            print("Wrong input")
+        
+        message = binascii.unhexlify(self.colors[chg])
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        s.send(message)
+        s.close()
 
-ch = input('1: on\n2: off\n3: Swithc color\n')
-
-if ch == '1':
-    message = binascii.unhexlify(on)
-elif ch == '2':
-    message = binascii.unhexlify(off)
-elif ch == '3':
-    message = binascii.unhexlify(switchcolor)
-else:
-    print("Error")
-    exit()
-
-print(message)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip, port))
-s.send(message)
-s.close()
+c = controler()
+c.control('192.168.1.100')
